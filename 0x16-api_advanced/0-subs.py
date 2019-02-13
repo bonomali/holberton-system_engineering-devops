6fa2d1a65669009b6715f16d629f689e5c5f85f2
eboayue <keboayue@gmail.com>
2019-02-13 14:03:28 -0800

    Change back to previous

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,19 +1,20 @@
 #!/usr/bin/python3
 """ Returns the number of subs """
+import json
 import requests
 
-if __name__ == "__main__":
-    def number_of_subscribers(subreddit):
-        """Queries the Reddit API and returns number of subs
-        Attributes:
-            subreddit (string): name of the subreddit
-        Return: The number of subscribers
-        """
-        url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
-        header = {"user-agent": "sub_count_test"}
 
-        try:
-            return (requests.get(url, headers=header, allow_redirects=False)
-                    .json()["data"]["subscribers"])
-        except:
-            return 0
+def number_of_subscribers(subreddit):
+    """Queries the Reddit API and returns number of subs
+    Attributes:
+        subreddit (string): name of the subreddit
+    Return: The number of subscribers
+    """
+    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
+    header = {"user-agent": "sub_count_test"}
+
+    try:
+        return (requests.get(url, headers=header, allow_redirects=False)
+                .json()["data"]["subscribers"])
+    except:
+        return 0
--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -1,22 +1,23 @@
 #!/usr/bin/python3
 """ Prints the titles of the first 10 posts """
+import json
 import requests
 
-if __name__ == "__main__":
-    def top_ten(subreddit):
-        """Queries the Reddit API and returns titles of top ten hot posts
-        Attributes:
-            subreddit (string): name of the subreddit
-        Return: titles of hot posts
-        """
-        url = "https://www.reddit.com/r/{}/hot.json?limit=10".format(subreddit)
-        header = {"user-agent": "top_ten_test"}
-        posts = requests.get(
-            url, headers=header, allow_redirects=False)\
-            .json()["data"]["children"]
 
-        try:
-            for post in posts:
-                print("{}".format(post["data"]["title"]))
-        except:
-            print("None")
+def top_ten(subreddit):
+    """Queries the Reddit API and returns titles of top ten hot posts
+    Attributes:
+        subreddit (string): name of the subreddit
+    Return: titles of hot posts
+    """
+    url = "https://www.reddit.com/r/{}/hot.json?limit=10".format(subreddit)
+    header = {"user-agent": "top_ten_test"}
+    posts = requests.get(
+        url, headers=header, allow_redirects=False)\
+        .json()["data"]["children"]
+
+    try:
+        for post in posts:
+            print("{}".format(post["data"]["title"]))
+    except:
+        print("None")
