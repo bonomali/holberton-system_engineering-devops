c77c38519d03fd7fe5486265655e67c714f84293
eboayue <keboayue@gmail.com>
2019-02-13 13:58:48 -0800

    Add if name == main

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -2,18 +2,18 @@
 """ Returns the number of subs """
 import requests
 
+if __name__ == "__main__":
+    def number_of_subscribers(subreddit):
+        """Queries the Reddit API and returns number of subs
+        Attributes:
+            subreddit (string): name of the subreddit
+        Return: The number of subscribers
+        """
+        url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
+        header = {"user-agent": "sub_count_test"}
 
-def number_of_subscribers(subreddit):
-    """Queries the Reddit API and returns number of subs
-    Attributes:
-        subreddit (string): name of the subreddit
-    Return: The number of subscribers
-    """
-    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
-    header = {"user-agent": "sub_count_test"}
-
-    try:
-        return (requests.get(url, headers=header, allow_redirects=False)
-                .json()["data"]["subscribers"])
-    except:
-        return 0
+        try:
+            return (requests.get(url, headers=header, allow_redirects=False)
+                    .json()["data"]["subscribers"])
+        except:
+            return 0
